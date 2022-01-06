7e7b0c56356f1f56de00287354f23de0574e275a
junaidAhmed8819 <junaidahmed882000@gmail.com>
2022-01-06 09:29:17 +0530

    Add project files.

---
+++ IService1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServerDatabse
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    { 
+        [OperationContract]
+        List<Employee> getAllEmployees();
+
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Employee
+    {
+        string name;
+        int empid;
+        double salary;
+
+        [DataMember]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [DataMember]
+        public int EmpId
+        {
+            get { return empid; }
+            set { empid = value; }
+        }
+
+        [DataMember]
+        public double Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+    }
+}
+
+
+
---
+++ Service1.svc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServerDatabse
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public List<Employee> getAllEmployees()
+        {
+            var EmployeeList = new List<Employee>();
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\91879\source\repos\WcfServerDatabse\App_Data\Database1.mdf;Integrated Security=True");
+
+            SqlCommand com = new SqlCommand("Select * from Employee;", con);
+            con.Open();
+            SqlDataReader reader = com.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Employee obj = new Employee();
+                    obj.EmpId = reader.GetInt32(0);
+                    obj.Name = reader.GetString(1);
+                    obj.Salary = reader.GetDouble(2);
+                    EmployeeList.Add(obj);
+                }
+            }
+            else
+            {
+                //No Reader
+            }
+            reader.Close();
+            return EmployeeList;
+        }
+
+    }
+}
